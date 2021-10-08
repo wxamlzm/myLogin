a728c0d3d3363a23efd923eeb719dcf7158eb5c7
wxamlzm <zd5024@163.com>
2021-10-08 20:46:15 +0800

    UPDATE:jwt-token鉴权初始化完成！

--- app/router/user.js
+++ app/router/user.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const User = require('../model/User')
 const router = express.Router()
 
+// 加密加盐
+// const salt = 'defdaczv'
+
+const secret = 'register-rule'
+
 // 定义验证身份中间件
-const isAdmin = async(req, res, next) => {
+const isAdmin = async (req, res, next) => {
     // console.log(req.headers.authorization)
+
+    /*
     // 1.获取token
     const token = req.headers.authorization.split(' ')[1]
     const id = token.split('.')[0]
     const username = token.split('.')[1]
-    // console.log(token, id, user)
+    */
+    
+    // jwt-token
+    const token = req.headers.authorization.split(' ').pop()
+    // console.log(jwt.verify(token, secret))
+
+    // 查询用户是否存在
+    const { _id, username } = jwt.verify(token, secret)
+
     // 2.查询用户是否存在
-    const user = await User.findById(id)
+    const user = await User.findById(_id)
+
     if(!user){ return res.status(422).send('用户错误') }
     // 3.查看username
     if(username !== user.username){
@@ -64,34 +81,55 @@ router.post('/login', async (req, res) => {
     let isPassword = bcrypt.compareSync(req.body.password, user.password)
     
     // 返回token
-    const token = user._id + '.' + user.username
+    // const token = user._id + '.' + user.username
+
+    // jwt-token
+    const { _id, username } = user
+    const token = jwt.sign(
+        { _id, username },     // 传值规则
+        secret,                // 加密规则
+        { expiresIn: '24h' }   // 时间（有效时间？）
+    )
+
     res.send(token)
 })
 
-/*
+
 // 验证
 router.get('/verify', async (req, res) => {
-    // console.log(req.headers.authorization)
+    console.log(req.headers.authorization)
     // 1.获取token
-    const token = req.headers.authorization.split(' ')[1]
-    const id = token.split('.')[0]
-    const username = token.split('.')[1]
-    // console.log(token, id, user)
-    // 2.查询用户是否存在
-    const user = await User.findById(id)
-    if(!user){ return res.status(422).send('用户错误') }
-    // 3.查看username
-    if(username !== user.username){
-        res.status(422).send('用户错误')
-    }else{
-        // 用户存在，查看权限
-        if(user.isAdmin === '0'){
-            res.status(409).send('没有权限')
-        }else if(user.isAdmin === '1'){
-            res.send('Admin')
-        }
-    }
+    // const token = req.headers.authorization.split(' ')[1]
+    // const id = token.split('.')[0]
+    // const username = token.split('.')[1]
+    // // console.log(token, id, user)
+    // // 2.查询用户是否存在
+    // const user = await User.findById(id)
+    // if(!user){ return res.status(422).send('用户错误') }
+    // // 3.查看username
+    // if(username !== user.username){
+    //     res.status(422).send('用户错误')
+    // }else{
+    //     // 用户存在，查看权限
+    //     if(user.isAdmin === '0'){
+    //         res.status(409).send('没有权限')
+    //     }else if(user.isAdmin === '1'){
+    //         res.send('Admin')
+    //     }
+    // }
+
+    /*
+    // jwt
+    const token = req.headers.authorization.split(' ').pop()
+    console.log(jwt.verify(token, secret))
+
+    // 查询用户是否存在
+    const { _id, username } = jwt.verify(token, secret)
+    console.log(_id)
+    */
+
+    
 })
-*/
+
 
 module.exports = router
