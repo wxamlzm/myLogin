b265ce9a071113a7ac12185874564ed04e59f6f5
wxamlzm <zd5024@163.com>
2021-10-08 17:58:00 +0800

    UPDATE:改写接口为中间件方法

--- app/router/user.js
+++ app/router/user.js
@@ -2,8 +2,33 @@ const express = require('express')
 const User = require('../model/User')
 const router = express.Router()
 
+// 定义验证身份中间件
+const isAdmin = async(req, res, next) => {
+    // console.log(req.headers.authorization)
+    // 1.获取token
+    const token = req.headers.authorization.split(' ')[1]
+    const id = token.split('.')[0]
+    const username = token.split('.')[1]
+    // console.log(token, id, user)
+    // 2.查询用户是否存在
+    const user = await User.findById(id)
+    if(!user){ return res.status(422).send('用户错误') }
+    // 3.查看username
+    if(username !== user.username){
+        res.status(422).send('用户错误')
+    }else{
+        // 用户存在，查看权限
+        if(user.isAdmin === '0'){
+            res.status(409).send('没有权限')
+        }else if(user.isAdmin === '1'){
+            // res.send('Admin')
+            next()
+        }
+    }
+}
+
 // 获取用户列表
-router.get('/', async (req, res) => {
+router.get('/', isAdmin, async (req, res) => {
     // res.send('hello userRouter')
     const list = await User.find()
     res.send(list)
@@ -41,6 +66,7 @@ router.post('/login', async (req, res) => {
     res.send(token)
 })
 
+/*
 // 验证
 router.get('/verify', async (req, res) => {
     // console.log(req.headers.authorization)
@@ -64,5 +90,6 @@ router.get('/verify', async (req, res) => {
         }
     }
 })
+*/
 
 module.exports = router
