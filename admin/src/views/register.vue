c832bd30289f0fa817363c50c8f407f77ea1815e
wxamlzm <zd5024@163.com>
2021-10-09 11:02:21 +0800

    UPDATE:注册页面初始化完成

--- admin/src/views/register.vue
+++ admin/src/views/register.vue
@@ -1,15 +1,96 @@
 <template>
-    <div>
-        register
+    <div class="register-box">
+        <h3>注册页面</h3>
+        <el-form :model="registerData" :rules="rules" ref="registerForm" label-width="100px" class="demo-ruleForm">
+            <el-form-item label="账号" prop="username">
+                <el-input v-model="registerData.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="password">
+                <el-input type="password" v-model="registerData.password"></el-input>
+            </el-form-item>
+            <el-form-item label="确认密码" prop="password2">
+                <el-input type="password" v-model="registerData.password2"></el-input>
+            </el-form-item>
+            <el-form-item label="用户名" prop="name">
+                <el-input v-model="registerData.name"></el-input>
+            </el-form-item>
+            <div class="input-button">
+                <el-button type="primary" @click="submitForm('registerForm')">注册</el-button>
+            </div>
+        </el-form>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        var validatePass = (rule, value, callback) => {
+            if (value !== this.registerData.password) {
+                callback(new Error('两次输入密码不一致!'));
+            } else {
+                callback();
+            }
+        };
+        return{
+            registerData: {
+                username: '',
+                password: '',
+                password2: ''
+            },
+            rules: {
+                username: [{required: true, message: '账号不得为空', trigger: 'blur'},
+                            {min: 3, max: 32, message: '长度应该在3-32之间', tigger: 'blur'}
+                ],
+                password: [{required: true, message: '密码不得为空', trigger: 'blur'},
+                           {min: 3, max: 32, message:'长度应该在3-32之间', tigger: 'blur'}
+                ],
+                password2: [{required: true, message: '确认密码不得为空', trigger: 'blur'},
+                            {min: 3, max: 32, message:'长度应该在3-32之间', tigger: 'blur'},
+                            {validator: validatePass, trigger: 'blur'}
+                ],
+                name: [{required: true, message: '用户名不得为空', trigger: 'blur'},
+                       {min: 3, max: 32, message: '长度应该在3-32之间', tigger: 'blur'}
+                ]
+            }
+        }
+    },
+    methods: {
+        submitForm(formName) {
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    alert('submit!');
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
+        },
+    }
 }
 </script>
 
 <style lang="scss">
+.register-box {
+    width: 500px;
+    height: 400px;
+    margin: 100px auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    box-shadow: 0 0 5px #ccc;
 
+    h3 {
+        text-align: center;
+    }
+    .el-input {
+        width: 92%;
+    }
+    .input-button {
+        display: flex;
+        button {
+            width: 120px;
+            margin: 0 auto;
+        }
+    }
+}
 </style>
