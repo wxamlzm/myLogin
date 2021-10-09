a8729d963d6375674fb79174151f8ead5e62420d
wxamlzm <zd5024@163.com>
2021-10-09 09:43:57 +0800

    UPDATE:页面初始化

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,22 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Register from '../views/register.vue'
+import Index from '../views/index.vue'
+import Login from '../views/login.vue'
+import NotFound from '../views/404.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/index'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/index',
+    name: 'index',
+    component: Index
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '*',
+    name: '404',
+    component: NotFound
   }
 ]
 
